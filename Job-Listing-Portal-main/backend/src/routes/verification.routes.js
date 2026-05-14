3c972b3433b1006a32ed8cbf9d4845e22718cdda
Aryan-20-5176 <cyber.aryan20@gmail.com>
2026-05-14 19:28:40 +0530

    remove duplicate folder

--- Job-Listing-Portal-main/backend/src/routes/verification.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const auth = require("../middlewares/auth.middleware");
-const { requestVerification } = require("../controllers/verification.controller");
-
-router.post("/request", auth, requestVerification);
-
-module.exports = router;
